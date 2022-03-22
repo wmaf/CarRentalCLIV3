12ed04f6914e12367709240565e3fbdb9b739a36
wmaf <wmaf@outlook.com>
2022-03-21 21:14:17 -0400

    Initial commit

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,40 @@
+package com.careerdevs;
+// Define a public class so code can be accessed throughout the program.
+public class Car {
+    // Make private so the is not visible outside of the method.
+    private String carMake;
+    private String carModel;
+    private int carYear;  // Used carYear vs year as year is a reserved word.
+
+    public Car(String carMake, String carModel, int carYear) {
+        this.carMake = carMake;
+        this.carModel = carModel;
+        this.carYear = carYear;
+
+    }
+
+    public String getCarMake() {
+        return carMake;
+    }
+
+    public void setCarMake(String carMake) {
+        this.carMake = carMake;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public int getCarYear() {
+        return carYear;
+    }
+
+    public void setCarYear(int carYear) {
+        this.carYear = carYear;
+    }
+}
+//use ctrl + insert to generate getters & setters.
